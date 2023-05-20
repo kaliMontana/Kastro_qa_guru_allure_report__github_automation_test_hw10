9ad3f62df135d3923531f90c30adc90695d5aedb
Silver <testingbugsrus@gmail.com>
2023-05-20 13:20:07 +0300

    Rename module

--- tests/test_ru.github.py
+++
@@ -1,4 +0,0 @@
-from selene.support.shared import browser
-
-def test_github():
-    browser.open("https://github.com/")
---
+++ tests/test_selene.py
@@ -0,0 +1,8 @@
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+
+
+def test_github():
+    browser.open('https://github.com/')
+
+    s('.header-search-input').click()
