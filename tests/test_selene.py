41463acd3dff98768660542ad94af7bcaadf572a
Silver <testingbugsrus@gmail.com>
2023-05-20 13:45:05 +0300

    Refactoring test_github

--- tests/test_selene.py
+++ tests/test_selene.py
@@ -1,8 +1,17 @@
+from selene import by, be
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
 
 
-def test_github():
+def test_github_selene():
     browser.open('https://github.com/')
 
     s('.header-search-input').click()
+    s('.header-search-input').send_keys('eroshenkoam/allure-example').click()
+    s('.header-search-input').submit()
+
+    s(by.link_text('eroshenkoam/allure-example')).click()
+
+    s('#issues-tab').click()
+
+    s(by.partial_text('#81')).should(be.visible)
