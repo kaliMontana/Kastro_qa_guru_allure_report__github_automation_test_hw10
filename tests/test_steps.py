b9150b6289fcbb58d5e3d8036f527d20e10c0bac
Silver <testingbugsrus@gmail.com>
2023-05-20 16:22:17 +0300

    - Add test test_decorator_steps

--- tests/test_steps.py
+++ tests/test_steps.py
@@ -4,6 +4,9 @@ from selene.support.conditions import be
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
 
+from pages.github_pages import open_main_page, search_for_repository, go_to_repository, open_issue_tab, \
+    should_see_issue_with_number
+
 
 def test_lamda_steps():
     with allure.step('Открыть главную страницу'):
@@ -22,3 +25,11 @@ def test_lamda_steps():
 
     with allure.step('Проверить название Issue с номером 81'):
         s(by.partial_text('#81')).should(be.visible)
+
+
+def test_decorator_steps():
+    open_main_page()
+    search_for_repository('eroshenkoam/allure-example')
+    go_to_repository('eroshenkoam/allure-example')
+    open_issue_tab()
+    should_see_issue_with_number('81')
